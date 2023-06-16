e9ffbe44938ace5ad61a4b88ee62fe4f4fe685ff
Logan <agj017@gmail.com>
2023-06-16 09:24:27 +0900

    chore: clean up code

--- jpa-postgre-spring-kotlin/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "jpa-postgre-spring-kotlin"
--- jpa-postgre-spring-kotlin/src/main/kotlin/com/example/jpapostgrespringkotlin/AccountEntityRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jpapostgrespringkotlin
-
-import java.util.UUID
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AccountEntityRepository: JpaRepository<Account, UUID>
