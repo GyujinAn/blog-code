f6592a7dc91d72a2119aa897891f0b2e232b7f4e
Logan <agj017@gmail.com>
2023-10-18 21:17:03 +0900

    add spring security

---
+++ spring/security/src/main/java/com/example/security/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity(debug = true)
+@Configuration
+public class SecurityConfiguration {
+
+}
---
+++ spring/security/src/test/java/com/example/security/SecurityConfigurationTest.java
@@ -0,0 +1,83 @@
+package com.example.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.Filter;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class SecurityConfigurationTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired
+    ConfigurableListableBeanFactory configurableListableBeanFactory;
+
+    @EnableWebSecurity(debug = true)
+    @TestConfiguration
+    public static class TestSecurityConfiguration {
+
+        @Bean
+        SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//            http.csrf().disable()
+//                    .httpBasic().disable()
+//                    .sessionManagement().disable()
+//                    .logout().disable()
+//                    .requestCache().disable()
+//                    .securityContext().disable();
+
+
+//            http.oauth2ResourceServer()
+//                    .jwt()
+//                    .jwtAuthenticationConverter(jwtAuthenticationConverter());
+            return http.build();
+        }
+
+//        private JwtAuthenticationConverter jwtAuthenticationConverter() {
+//            JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+//            converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+//                List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+//                List<String> permissionList = jwt.getClaimAsStringList("PERMISSION_JWT_KEY");
+//                for (String permission : permissionList) {
+//                    authorities.add(new SimpleGrantedAuthority(permission));
+//                }
+//                return Collections.unmodifiableCollection(authorities);
+//            });
+//            return converter;
+//        }
+    }
+
+    @Test
+    public void check_security_filter_count() {
+
+        displayBeanDefinitionNames();
+
+        SecurityFilterChain filterChain = (SecurityFilterChain) context.getBean("filterChain");
+
+        List<Filter> filters = filterChain.getFilters();
+
+        assertEquals(11, filters.size());
+
+    }
+
+    private void displayBeanDefinitionNames() {
+        String[] beanDefinitionNames = configurableListableBeanFactory.getBeanDefinitionNames();
+
+        System.out.println("Display bean definition names");
+        for (String name : beanDefinitionNames) {
+            System.out.println(name);
+        }
+    }
+}
