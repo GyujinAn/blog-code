3300dc65773a0850cb3ccab2135a4f67347da0ec
Logan <agj017@gmail.com>
2023-10-19 13:57:37 +0900

    spring configuration is replaced by spring core

--- spring/core/src/main/java/com/example/core/MyConfiguration.java
+++ spring/core/src/main/java/com/example/core/MyConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.configuration;
+package com.example.core;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- spring/core/src/main/java/com/example/core/MyConfigurationProperties.java
+++ spring/core/src/main/java/com/example/core/MyConfigurationProperties.java
@@ -1,4 +1,4 @@
-package com.example.configuration;
+package com.example.core;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
