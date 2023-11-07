0c24989af89df36363c8d18d07ae24cde50d1138
gyujinan <agj017@gmail.com>
2023-11-08 07:37:50 +0900

    add contents to about-pointer.c

--- c/about-pointer.c
+++ c/about-pointer.c
@@ -26,10 +26,67 @@ void PrintPointerSizeAndAddress() {
     printf("=== end PrintPointerSizeAndAddress ===\n\n");
 }
 
+void ArrayIsPointer() {
+    printf("=== start ArrayIsPointer ===\n");
+
+    int arr[3] = {0, 1, 2};
+    int * ptr = &arr[0];
+
+    printf("array variable: %p \n", arr);
+    printf("first element: %p \n", &arr[0]);
+    printf("second element: %p \n", &arr[1]);
+    printf("third element: %p \n", &arr[2]);
+
+    printf("%d %d \n", ptr[0], arr[0]);
+    printf("%d %d \n", ptr[1], arr[1]);
+    printf("%d %d \n", ptr[2], arr[2]);
+    printf("%d %d \n", *ptr, *arr);
+
+    printf("=== end ArrayIsPointer ===\n\n");
+
+}
+
+void PointerBaseArrayAccess() {
+    printf("=== start PointerBaseArrayAccess ===\n");
+
+    int arr[3] = {11, 22, 33};
+    int * ptr = arr;
+    printf("%d %d %d \n", *ptr, *(ptr+1), *(ptr+2));
+
+    printf("%d ", *ptr); ptr++;
+    printf("%d ", *ptr); ptr++;
+    printf("%d ", *ptr); ptr--;
+    printf("%d ", *ptr); ptr--;
+    printf("%d ", *ptr); printf("\n");
+
+    printf("=== end PointerBaseArrayAccess ===\n\n");
+}
+
+void TowStringType() {
+    printf("=== start TowStringType ===\n");
+
+    char str1[]="Hello";
+    char * str2="World";
+    printf("%s, %s \n", str1, str2);
+
+    str1[0]='X';
+    str2[0]='X'; // runtime err
+
+    printf("%s, %s \n", str1, str2);
+    
+    printf("=== end TowStringType ===\n\n");
+}
+
 int main(int argc, char const *argv[])
 {
 
     PrintPointerSizeAndAddress();
-    
+
+    ArrayIsPointer();
+
+    PointerBaseArrayAccess();
+
+    TowStringType();
+
     return 0;
 }
