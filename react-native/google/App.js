0ffc87360e2e225645279671649aa63abcab8334
JIN <agj017@gmail.com>
2023-11-24 17:26:06 +0900

    clean up code

--- react-native/google/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import GooglePlacesInput from './components/GooglePlacesInput';
-
-function App() {
-  return <GooglePlacesInput />;
-}
-
-export default App;
