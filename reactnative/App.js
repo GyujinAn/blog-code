58cbbb603e6b2f52dac0280eb17c0b08301b6717
JIN <agj017@gmail.com>
2023-12-05 15:59:23 +0900

    set up firestore for android

--- reactnative/App.js
+++ reactnative/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import UploadStack from './practice/upload/UploadStack';
 import BorderedInput from './components/BorderedInput';
+import {Button} from 'react-native';
+import {createPost} from './lib/posts';
+
+const testCreatePost = async () => {
+  console.log('testCreatePost is started');
+  const org = {
+    hello: 'world',
+  };
+  await createPost({org});
+};
 
 function App() {
-  return <UploadStack />;
+  // return <UploadStack />;
   // return <BorderedInput />;
+  return <Button title="Press me" onPress={testCreatePost} />;
 }
 export default App;
