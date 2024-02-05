620809239fede8eeaa7be3e99d283d5f9fcd2aec
Christopher <agj017@gmail.com>
2024-02-05 11:04:12 +0900

    add operator over loading of c++

--- cpp/about-operator-overloading.cpp
+++ cpp/about-operator-overloading.cpp
@@ -29,14 +29,57 @@ public:
         return *this;
     }
 
+    Point operator++(int)
+    {
+        const Point retobj(xpos, ypos);
+        xpos += 1;
+        ypos += 1;
+        return retobj;
+    }
+
+    Point operator*(int times)
+    {
+        Point pos(xpos * times, ypos * times);
+        return pos;
+    }
+
+    friend Point operator-(const Point &pos1, const Point &pos2);
     friend Point &operator--(Point &ref);
+    friend const Point operator--(Point &ref, int);
+    friend Point operator*(int times, Point &ref);
+    friend ostream &operator<<(ostream &, const Point &);
 };
+
+Point operator-(const Point &pos1, const Point &pos2)
+{
+    Point pos(pos1.xpos - pos2.xpos, pos1.ypos - pos2.ypos);
+    return pos;
+}
 
 Point &operator--(Point &ref)
 {
     ref.xpos -= 1;
     ref.ypos -= 1;
     return ref;
+}
+
+const Point operator--(Point &ref, int)
+{
+    const Point retobj(ref);
+    ref.xpos -= 1;
+    ref.ypos -= 1;
+    return retobj;
+}
+
+Point operator*(int times, Point &ref)
+{
+    return ref * times;
+}
+
+ostream &operator<<(ostream &os, const Point &pos)
+{
+    os << '[' << pos.xpos << ", " << pos.ypos << ']' << endl;
+    return os;
 }
 
 void showBinaryOperatorOverloading()
@@ -64,9 +107,50 @@ void showUnaryOperatorOverloading()
     pos.ShowPosition();
 }
 
+void showPosteriorOperatorOverloading()
+{
+    Point pos(3, 5);
+    Point cpy;
+    cpy = pos--;
+    cpy.ShowPosition();
+    pos.ShowPosition();
+
+    cpy = pos++;
+    cpy.ShowPosition();
+    pos.ShowPosition();
+}
+
+void showCommutativeOperatorOverloading()
+{
+    Point pos(1, 2);
+    Point cpy;
+
+    cpy = 3 * pos;
+    cpy.ShowPosition();
+
+    cpy = 2 * pos * 3;
+    cpy.ShowPosition();
+}
+
+void showCoutOperatorOverloading()
+{
+    Point pos1(1, 3);
+    cout << pos1;
+    Point pos2(101, 303);
+    cout << pos2;
+}
+
 int main(void)
 {
     // showBinaryOperatorOverloading();
 
-    showUnaryOperatorOverloading();
+    // showUnaryOperatorOverloading();
+
+    // showPosteriorOperatorOverloading();
+
+    // showCommutativeOperatorOverloading();
+
+    showCoutOperatorOverloading();
+
+    return 0;
 }
