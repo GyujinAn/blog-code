a7b54f66b7dcdd6d4f01a9d7954c6e77ee867b05
Christopher <agj017@gmail.com>
2024-05-14 08:50:22 +0900

    reinforce java/src/algorithm/euclideanmethod/EuclideanMethod.java

--- java/src/algorithm/euclideanmethod/EuclideanMethod.java
+++ java/src/algorithm/euclideanmethod/EuclideanMethod.java
@@ -8,7 +8,24 @@ public class EuclideanMethod {
         }
     }
 
+    static int calculateGreatestCommonDivisor(int [] array) {
+        int result = array[0];
+
+        for (int i = 1; i < array.length; i ++) {
+            result = calculateGreatestCommonDivisor(result, array[i]);
+
+            if (result == 1) {
+                return result;
+            }
+        }
+
+        return result;
+    }    
+
     public static void main(String[] args) {
         System.out.println(calculateGreatestCommonDivisor(22,8));
+
+        int [] array = {3 ,9, 12};
+        System.out.println(calculateGreatestCommonDivisor(array));
     }
 }
